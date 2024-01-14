47df38a7fbb4f20f78a4f6a8523ad0da352abb9c
Rishabh Jain <rishabhjain4891@gmail.com>
2024-01-14 14:56:07 +0530

    added comment nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
 
-    opts={
+    opts = {
       auto_install = true,
     }
 
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,11 @@ return {
       null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.markdownlint,
+      null_ls.builtins.formatting.cbfmt,
+      null_ls.builtins.formatting.mdformat,
+      null_ls.builtins.diagnostics.markdownlint_cli2,
+      null_ls.builtins.formatting.prettier,
     })
 
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
