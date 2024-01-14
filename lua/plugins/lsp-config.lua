5c7a1ec37807bf90e849e1eaa37d6577611c2c0a
Rishabh Desktop <rishabhjain4891@gmail.com>
2024-01-14 13:57:46 +0530

    Added Completions lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,44 @@
 return {
   {
     "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
-  },
-  {
-  "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls","marksman","gopls"}
-      })
+      require("mason").setup()
     end
   },
   {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+
+    opts={
+      auto_install = true,
+    }
+
+  },
+  {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
-      local opts = {}
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup{}
-      lspconfig.marksman.setup{}
-      vim.keymap.set('n','K', vim.lsp.buf.hover,{})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.marksman.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
