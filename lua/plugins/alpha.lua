0a3ec20151ff000492e1dfdb35a8f220eb749ac0
Rishabh Desktop <rishabhjain4891@gmail.com>
2024-01-14 19:00:15 +0530

    Added debugging

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+    dashboard.section.position = "center"
+    dashboard.section.header.opts.position = "center"
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
